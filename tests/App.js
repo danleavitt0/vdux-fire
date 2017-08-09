79c3d109de3341f6dc8b6d97ef4ccc13bdac5ccb
danleavitt0 <danleavitt0@gmail.com>
2017-08-09 16:19:25 -0700

    add example

---
+++ tests/App.js
@@ -0,0 +1,55 @@
+import {component, element} from 'vdux'
+import mapValues from '@f/map-values'
+import map from '@f/map'
+import fire from '..'
+
+export default fire(({classRef, playlistRef, uid}) => ({
+ activity: {
+   ref: `/classes/${classRef}`,
+   join: {
+     ref: '/playlistInstances',
+     child: 'students',
+     childRef: (val, ref) => {
+      	return map((u, k) => ref.root
+         .child('/playlistsByUser')
+         .child(k)
+         .child('byPlaylistRef')
+         .child(playlistRef)
+         .child('instanceRef')
+         .once('value')
+         .then(s => s.val())
+         .then(iref => ref.child(iref)), val.students)
+
+       // return Promise.all(p)
+       //   .then(snaps => snaps.map(s => {console.log(s.val(), s.key); return s.val()}).filter(s => !!s))
+       //   .then(instanceRefs => instanceRefs.map(iref => ref.child(iref.instanceRef)))
+     }
+   }
+ },
+ myProgress: {
+    ref: `/playlistsByUser/${uid}/byPlaylistRef/${playlistRef}`,
+    join: {
+      ref: `/playlistInstances`,
+      child: `instanceRef`,
+      // childRef: (val, ref) => {
+      //   return ref.child(val.instanceRef)
+      // }
+    }
+  },
+  inProgress: {
+		ref: `/playlistsByUser/${uid}/inProgress#orderByChild=lastEdited`,
+		join: {
+			ref: '/playlistInstances',
+			child: 'playlistValue',
+			childRef: (val, ref) => val.map(v => ref.child(v.key))
+		}
+	},
+}))(component({
+	render ({props}) {
+		const {activity, myProgress, inProgress} = props
+		if (activity.loading) return <span/>
+		return (
+			<div> Hello World </div>
+		)
+	}
+}))
---
+++ tests/Boot.js
@@ -0,0 +1,110 @@
+/**
+ * Imports
+ */
+
+import {
+  middleware as firebaseMw,
+  set as firebaseSet,
+  update as firebaseUpdate,
+  once as firebaseOnce,
+  push as firebasePush,
+  transaction
+} from '..'
+import {component, element} from 'vdux'
+import App from './App'
+import map from '@f/map'
+
+/**
+ * Constants
+ */
+
+const apiServer = process.env.API_SERVER
+const firebaseConfig = {
+	"apiKey": "AIzaSyAQ7YJxZruXp5RhMetYq1idFJ8-y0svN-s",
+	"authDomain":"artbot-dev.firebaseapp.com",
+	"databaseURL":"https://artbot-dev.firebaseio.com",
+	"storageBucket":"artbot-dev.appspot.com",
+	"messagingSenderId":"1001646388415"
+}
+
+/**
+ * <Boot/>
+ */
+
+export default component({
+
+  initialState ({props}) {
+    if (props.req) {
+      return {
+        currentUrl: props.req.url
+      }
+    }
+
+    return {}
+  },
+
+  getContext ({props, state, actions}) {
+    const {media, currentUrl, avatarUpdates} = state
+    const userAgent = props.req
+      ? props.req.headers['user-agent']
+      : window.navigator.userAgent
+
+    return {
+      ...actions
+    }
+  },
+
+  render ({props, state}) {
+    return <App uid='A7rojVypFFNCYQb2A22wM189HWL2' classRef='-Kqk0NoS5lywwzissGvg' playlistRef='-Ke7eny8WMLluXLz3Ii5' {...state} {...props} />
+  },
+
+  onUpdate (prev, next) {
+    if (prev.state.title !== next.state.title && typeof document !== 'undefined') {
+      document.title = next.state.title
+    }
+  },
+
+  middleware: [firebaseMw(firebaseConfig)],
+
+  controller: {
+    firebaseSet: wrapEffect(firebaseSet),
+    firebaseUpdate: wrapEffect(firebaseUpdate),
+    firebaseOnce: wrapEffect(firebaseOnce),
+    firebaseTransaction: wrapEffect(transaction),
+    firebasePush: wrapEffect(firebasePush)
+  },
+
+  reducer: {
+    updateAvatar: state => ({
+      avatarUpdates: (state.avatarUpdates || 0) + 1
+    }),
+    updateMedia: (state, {key, matches}) => ({
+      media: state.media === key
+        ? matches ? key : null
+        : matches ? key : state.media
+    }),
+    updateUrl: (state, currentUrl) => ({
+      currentUrl,
+      modal: state.currentUrl === currentUrl ? state.modal : null
+    }),
+    openModal: (state, modal) => ({
+      // Dont allow modal opening on the server, because it causes
+      // issues with server-side rendering
+      modal: typeof window === 'undefined' ? null : modal
+    }),
+    closeModal: () => ({modal: null}),
+    showToast: (state, toast) => ({toast}),
+    hideToast: () => ({toast: null}),
+    setTitle: (state, title) => ({title}),
+    setUserId: (state, userId) => ({userId}),
+    setUsername: (state, username) => ({username})
+  }
+})
+
+/**
+ * Helpers
+ */
+
+function wrapEffect (fn) {
+  return (model, ...args) => fn(...args)
+}
